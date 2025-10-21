8073fc75723c57c058170f10792362a37747698d
Stanislav Zaikin <zstaseg@gmail.com>
2025-10-21 13:54:51 +0200

    ASoC: qcom: sm8250: Add Xiaomi Taoyao soundcard compatible
    Signed-off-by: Stanislav Zaikin <zstaseg@gmail.com>

--- sound/soc/qcom/sm8250.c
+++ sound/soc/qcom/sm8250.c
@@ -213,6 +213,7 @@ static const struct of_device_id snd_sm8250_dt_match[] = {
 	{ .compatible = "qcom,qrb4210-rb2-sndcard", .data = "sm4250" },
 	{ .compatible = "qcom,qrb5165-rb5-sndcard", .data = "sm8250" },
 	{ .compatible = "qcom,sm8250-sndcard", .data = "sm8250" },
+	{ .compatible = "xiaomi,taoyao-sndcard", .data = "sm7325" },
 	{}
 };
 
